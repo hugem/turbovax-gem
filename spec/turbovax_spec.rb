da98f02287370639d08588767fa361413a9aaff4
hugem <huge.ma@gmail.com>
2021-04-23 14:39:04 -0400

    fix spec

--- spec/turbovax_spec.rb
+++ spec/turbovax_spec.rb
@@ -5,7 +5,24 @@ RSpec.describe Turbovax do
     expect(Turbovax::VERSION).not_to be nil
   end
 
-  it "does not error" do
-    expect(false).to eq(true)
+  it "can configure without error" do
+    expect do
+      Turbovax.configure do |config|
+        config.logger = Logger.new($stdout, level: Logger::DEBUG)
+        config.twitter_enabled = true
+        config.twitter_credentials = {
+          consumer_key: "CONSUMER_KEY",
+          consumer_secret: "CONSUMER_SECRET",
+          access_token: "ACCESS_TOKEN",
+          access_token_secret: "ACCESS_TOKEN_SECRET"
+        }
+
+        config.faraday_logging_config = {
+          headers: true,
+          bodies: true,
+          log_level: :info
+        }
+      end
+    end.not_to raise_error
   end
 end
